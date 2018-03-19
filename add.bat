9160948e39831e40ba88a4ebcc3fe450097ebb78
honey data <16432726+honeydatax@users.noreply.github.com>
2018-03-19 19:01:13 +0000

    add client to client.db data base

--- add.bat
+++ add.bat
@@ -1,16 +1,16 @@
 @echo off
-cls
+color 15 1
 echo INSERT INTO list ( NAME,PHONE,ADDRESS)>inp.txt
-echo VALUES (>>inp.txt
-echo "client name"
+echo VALUES ( >> inp.txt
+prints "client name"
 input inp.txt
-echo , >>inp.txt
-echo "client phone"
+echo , >> inp.txt
+prints "client phone"
 input inp.txt
-echo , >>inp.txt
-echo "client address"
+echo ,  >> inp.txt
+prints "client address"
 input inp.txt
-echo ) ; >>inp.txt
+echo ) ;  >> inp.txt
 echo .quit >>inp.txt
 
 sqlite3 client.db < inp.txt 
