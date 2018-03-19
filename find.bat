3390efe26726c2f98671669ab61ab85b391f0ae2
honey data <16432726+honeydatax@users.noreply.github.com>
2018-03-19 19:53:57 +0000

    find a client where part of a name

--- find.bat
+++ find.bat
@@ -4,7 +4,7 @@ color 15 1
 echo .header on >inp.txt
 echo .mode  tabs>>inp.txt
 echo .width  3, 3 , 3 , 3 >>inp.txt
-echo SELECT * FROM list WHERE name= >>inp.txt
+echo SELECT * FROM list WHERE name like >>inp.txt
 
 prints "client name to find"
 input inp.txt
