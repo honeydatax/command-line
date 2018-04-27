a4f26825fc30815be2e118d6b016f4753fa522b9
honey data <16432726+honeydatax@users.noreply.github.com>
2018-04-27 19:17:41 +0000

    ibar.bat | ibar.exe

---
+++ ibar.bat
@@ -0,0 +1,51 @@
+@echo off
+
+echo 0 
+echo geting ready 
+sleep 2
+
+echo 10 
+echo p 10 
+sleep 2
+
+echo 20 
+echo p 30 
+sleep 2
+
+
+echo 30 
+echo p 30 
+sleep 2
+
+
+echo 40 
+echo p 40 
+sleep 2
+
+
+echo 50 
+echo p 50 
+sleep 2
+
+echo 60 
+echo p 60 
+sleep 2
+
+echo 70 
+echo p 70 
+sleep 2
+
+
+echo 80 
+echo p 80 
+sleep 2
+
+echo 90 
+echo p 90 
+sleep 2
+
+echo 100 
+echo p 100 
+sleep 2
+
+@echo on
