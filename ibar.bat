89aefe24e973c9b1b2b447a57a4c0af20d1145f2
honey data <16432726+honeydatax@users.noreply.github.com>
2018-04-28 11:37:43 +0000

    ibar.bat | ibar.exe ibar demo like more.exe head

--- ibar.bat
+++ ibar.bat
@@ -1,51 +1,12 @@
 @echo off
+cls
+echo .header on >inp.txt
+echo .mode  tabs>>inp.txt
+echo .width  3, 3 , 3 , 3 >>inp.txt
+echo select * from list>>inp.txt
+echo  ; >>inp.txt
+echo .quit >>inp.txt
 
-echo 0 
-echo geting ready 
-
-
-echo 10 
-echo p 10 
-
-
-echo 20 
-echo p 30 
-
-
-
-echo 30 
-echo p 30 
-
-
-
-echo 40 
-echo p 40 
-
-
-
-echo 50 
-echo p 50 
-
-
-echo 60 
-echo p 60 
-
-
-echo 70 
-echo p 70 
-
-
-
-echo 80 
-echo p 80 
-
-
-echo 90 
-echo p 90 
-
-
-echo 100 
-echo p 100 
-
-
-@echo on
+sqlite3 client.db < inp.txt  >out.txt
+type out.txt | ibar
+echo on
